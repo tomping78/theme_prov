056d0bba60c65d8cfeaf03c4a0edbb85a9baff3b
geunyoung01~ <screw24@nate.com>
2022-07-01 15:27:07 +0900

    theme update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Landing from "./Components/Landing/Landing";
+import { ThemeContext } from "./Theme";
 
 function App() {
+
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${theme}`}>
+      <Header />
+      <Landing />
     </div>
   );
 }
